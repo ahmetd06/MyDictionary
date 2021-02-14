7546c82d831e7dd10b417b5dc576ead98f98c7e3
Ahmet <ahmet.dundar@hotmail.com>
2021-02-14 17:02:30 +0300

    ilk

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MyDictionary
+{
+    public class MyDictionary<T1,T2>
+    {
+        T1[] _keys;
+        T2[] _values;
+        
+
+        public MyDictionary()
+        {
+            _keys = new T1[0];
+            _values = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] _keysTemp = _keys;
+            _keys = new T1[_keys.Length + 1];
+
+            for (int i = 0; i < _keysTemp.Length - 1; i++)
+            {
+                _keys[i] = _keysTemp[i];
+            }
+            _keys[_keys.Length - 1] = key;
+
+            T2[] _valuesTemp = _values;
+            _values = new T2[_values.Length + 1];
+
+            for (int i = 0; i < _valuesTemp.Length - 1; i++)
+            {
+                _values[i] = _valuesTemp[i];
+            }
+            _values[_values.Length - 1] = value;
+        }
+
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("Ahmet", 5);
+            myDictionary.Add("Hakan", 6);
+
+            
+        }
+    }
+}
